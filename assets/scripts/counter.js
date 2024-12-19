f719522549be409065b193f0d0576409ecbbe5c9
Vitalii Novosad <novosadvitalya@gmail.com>
2024-12-19 20:54:38 +0200

    Some improvements.
    1. Change header position to fixed.
    2. Add navigation features.

--- assets/scripts/counter.js
+++ assets/scripts/counter.js
@@ -86,3 +86,25 @@ updateCounter(false);
 
 // Запускаємо оновлення кожні 5 секунд
 setInterval(updateCounter, 2000);
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('header ul li a');
+
+      for (const link of links) {
+      link.addEventListener('click', function(e) {
+        e.preventDefault(); // Запобігаємо стандартному переходу
+
+        const targetId = this.getAttribute('href').substring(0);
+        const targetSection = document.getElementById(targetId);
+          console.log(targetId);
+
+        if (targetSection) {
+          // Плавна прокрутка до секції
+          window.scrollTo({
+            top: targetSection.offsetTop - document.querySelector('header').offsetHeight, // Враховуємо висоту шапки
+            behavior: 'smooth'
+          });
+        }
+      });
+    }
+  });
